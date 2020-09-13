67f4b91142fae7fc49a92cb2d7422993e723256a
Valentin Misiaszek <dev@misiaszek-valentin.com>
2020-09-13 14:03:41 +0200

    init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b2099076dcc60d9c9c2ff629bccc6e8f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dc5bc62b-5a88-4172-b5ba-1b785e3f51cb'
+};
